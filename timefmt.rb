ba8e7e7636199154b411dc51bf4e3b12e8c76da0
Patrick Jung <patrick.pddstudio@googlemail.com>
2018-12-03 06:13:07 +0100

    Update: Adjusted version to match with github releases

--- timefmt.rb
+++ timefmt.rb
@@ -8,7 +8,7 @@ class Timefmt < Formula
   desc "A tiny command line utility for time conversion"
   homepage "https://github.com/pddstudio/timefmt"
   url "https://github.com/PDDStudio/timefmt/releases/download/v1.1.1/timefmt-macos-v1.1.1.tar.gz"
-  version "v1.1.0"
+  version "v1.1.1"
   sha256 "b19f232dd352b8f928bab8c70ca37ec49b5024951d657f556888a10c487bed73"
   head "https://github.com/pddstudio/timefmt.git", :branch => "develop"
 
