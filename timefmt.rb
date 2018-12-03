b913c7f0869d71a222f331dbbd88f082880dc5d9
Patrick Jung <patrick.pddstudio@googlemail.com>
2018-12-03 02:19:29 +0100

    Added head section with reference to timefmt git repository

--- timefmt.rb
+++ timefmt.rb
@@ -10,6 +10,7 @@ class Timefmt < Formula
   url "https://github.com/PDDStudio/timefmt/releases/download/v1.1.1/timefmt-macos-v1.1.1.tar.gz"
   version "v1.1.0"
   sha256 "b19f232dd352b8f928bab8c70ca37ec49b5024951d657f556888a10c487bed73"
+  head "https://github.com/pddstudio/timefmt.git"
 
   def install
     brew.install "timefmt"
